3abd5c2b8932d5b95eb95ffc46779e0c60c35f0c
Eli Baynes <sbaynes1994@gmail.com>
2023-03-16 18:41:21 -0500

    add files from missed class

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,38 @@
+import { createBrowserRouter, Link, RouterProvider } from "react-router-dom";
+import Layout from "./layout";
+import Profiles from "./routes/profiles";
+import Thoughts from "./routes/thoughts";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/profiles",
+        element: <Profiles />,
+      },
+      {
+        path: "/thoughts",
+        element: <Thoughts />,
+      },
+    ],
+  },
+]);
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <RouterProvider router={router}>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/profiles">Profiles</Link>
+          </li>
+          <li>
+            <Link to="/thoughts">Thoughts</Link>
+          </li>
+        </ul>
+      </nav>
+    </RouterProvider>
+  );
 }
